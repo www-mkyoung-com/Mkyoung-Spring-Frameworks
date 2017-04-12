f70cd940334d07b22b4c5f78b25fffb05a48668e
mghasemy <mohammad.ghasemy@gmail.com>
2017-04-13 00:08:11 +0430

    add 2

---
+++ Spring Core Tutorials/Spring Quick Start/Spring 3 hello world example/src/main/java/com/mkyong/core/App.java
@@ -0,0 +1,14 @@
+package com.mkyong.core;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				"SpringBeans.xml");
+
+		HelloWorld obj = (HelloWorld) context.getBean("helloBean");
+		obj.printHello();
+	}
+}
