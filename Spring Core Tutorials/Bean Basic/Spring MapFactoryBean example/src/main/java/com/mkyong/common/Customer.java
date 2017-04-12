d7ccc48bbc3089d5552aab24e0cfa0a24b199794
mghasemy <mohammad.ghasemy@gmail.com>
2017-04-13 01:09:58 +0430

    add 11

---
+++ Spring Core Tutorials/Bean Basic/Spring MapFactoryBean example/src/main/java/com/mkyong/common/Customer.java
@@ -0,0 +1,23 @@
+package com.mkyong.common;
+
+import java.util.Map;
+
+public class Customer {
+
+	private Map maps;
+
+	public Map getMaps() {
+		return maps;
+	}
+
+	public void setMaps(Map maps) {
+		this.maps = maps;
+	}
+
+	@Override
+	public String toString() {
+		return "Customer [maps=" + maps + "]";
+	}
+
+	
+}
