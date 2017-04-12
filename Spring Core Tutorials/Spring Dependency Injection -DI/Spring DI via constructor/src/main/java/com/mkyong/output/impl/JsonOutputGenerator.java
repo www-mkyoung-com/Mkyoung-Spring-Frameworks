76acef8e424fdc99a336fbea45ec7d0e2949abaf
mghasemy <mohammad.ghasemy@gmail.com>
2017-04-13 00:18:55 +0430

    add 67

---
+++ Spring Core Tutorials/Spring Dependency Injection -DI/Spring DI via constructor/src/main/java/com/mkyong/output/impl/JsonOutputGenerator.java
@@ -0,0 +1,10 @@
+package com.mkyong.output.impl;
+ 
+import com.mkyong.output.IOutputGenerator;
+ 
+public class JsonOutputGenerator implements IOutputGenerator
+{
+	public void generateOutput(){
+		System.out.println("This is Json Output Generator");
+	}
+}
