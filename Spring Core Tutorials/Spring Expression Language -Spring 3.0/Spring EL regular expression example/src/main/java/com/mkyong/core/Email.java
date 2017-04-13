bc3fc75499d889f2aab131b9e603bb279910b395
mghasemy <mohammad.ghasemy@gmail.com>
2017-04-14 01:17:43 +0430

    ver 7

---
+++ Spring Core Tutorials/Spring Expression Language -Spring 3.0/Spring EL regular expression example/src/main/java/com/mkyong/core/Email.java
@@ -0,0 +1,20 @@
+package com.mkyong.core;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component("emailBean")
+public class Email {
+
+	@Value("nospam@abc.com")
+	String emailAddress;
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+
+}
