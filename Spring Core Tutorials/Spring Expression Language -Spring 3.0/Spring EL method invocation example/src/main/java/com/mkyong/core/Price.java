1371157ee8941d5d24a766f2e58db17c260cbb87
mghasemy <mohammad.ghasemy@gmail.com>
2017-04-14 01:12:23 +0430

    ver 3

---
+++ Spring Core Tutorials/Spring Expression Language -Spring 3.0/Spring EL method invocation example/src/main/java/com/mkyong/core/Price.java
@@ -0,0 +1,12 @@
+package com.mkyong.core;
+
+import org.springframework.stereotype.Component;
+
+@Component("priceBean")
+public class Price {
+
+	public double getSpecialPrice() {
+		return new Double(99.99);
+	}
+
+}
