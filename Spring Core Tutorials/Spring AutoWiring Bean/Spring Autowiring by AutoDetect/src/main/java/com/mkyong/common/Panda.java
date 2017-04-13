2c5d52110d366bc717c28367804f1ef48deaa252
mghasemy <mohammad.ghasemy@gmail.com>
2017-04-14 01:27:51 +0430

    ver 14

---
+++ Spring Core Tutorials/Spring AutoWiring Bean/Spring Autowiring by AutoDetect/src/main/java/com/mkyong/common/Panda.java
@@ -0,0 +1,30 @@
+package com.mkyong.common;
+
+public class Panda {
+	private KungFu kungfu;
+
+	/*public Panda(KungFu kungfu) {
+		System.out.println("autowiring by constructor");
+		this.kungfu = kungfu;
+	}*/
+
+	/*
+	public Panda(String kungfu) {
+		System.out.println("autowiring by constructor");
+	}*/
+	
+	public KungFu getKungfu() {
+		return kungfu;
+	}
+
+	public void setKungfu(KungFu kungfu) {
+		System.out.println("autowiring by type");
+		this.kungfu = kungfu;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [kungfu=" + kungfu + "]";
+	}
+
+}
