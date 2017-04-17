b4e08b3edba38d85227395f0ace475db168fcbfa
mghasemy <mohammad.ghasemy@gmail.com>
2017-04-17 13:27:03 +0430

    add v24

---
+++ Spring MVC Tutorials/Form Handling/Handling duplicate form submission in Spring MVC/src/main/java/com/mkyong/customer/model/Customer.java
@@ -0,0 +1,96 @@
+package com.mkyong.customer.model;
+
+public class Customer{
+	
+	//textbox
+	String userName;
+	
+	//textarea
+	String address;
+	
+	//password
+	String password;
+	String confirmPassword;
+	
+	//checkbox
+	boolean receiveNewsletter;
+	String [] favFramework;
+	
+	//radio button
+	String favNumber;
+	String sex;
+	
+	//dropdown box
+	String country;
+	String javaSkills;
+	
+	//hidden value
+	String secretValue;
+	
+	public String getSecretValue() {
+		return secretValue;
+	}
+	public void setSecretValue(String secretValue) {
+		this.secretValue = secretValue;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getConfirmPassword() {
+		return confirmPassword;
+	}
+	public void setConfirmPassword(String confirmPassword) {
+		this.confirmPassword = confirmPassword;
+	}
+	public boolean isReceiveNewsletter() {
+		return receiveNewsletter;
+	}
+	public void setReceiveNewsletter(boolean receiveNewsletter) {
+		this.receiveNewsletter = receiveNewsletter;
+	}
+	public String[] getFavFramework() {
+		return favFramework;
+	}
+	public void setFavFramework(String[] favFramework) {
+		this.favFramework = favFramework;
+	}
+	public String getFavNumber() {
+		return favNumber;
+	}
+	public void setFavNumber(String favNumber) {
+		this.favNumber = favNumber;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getJavaSkills() {
+		return javaSkills;
+	}
+	public void setJavaSkills(String javaSkills) {
+		this.javaSkills = javaSkills;
+	}
+}
