3481d986b7a6df8b761562ec1cd7a9d5dd34f9e0
mghasemy <mohammad.ghasemy@gmail.com>
2017-04-19 16:00:48 +0430

    mk5 v16

---
+++ Spring Batch Tutorials/ItemReader- ItemProcessor- ItemWriter/Spring Batch Example - CSV File To MySQL Database/src/main/java/com/mkyong/model/Report.java
@@ -0,0 +1,36 @@
+package com.mkyong.model;
+
+public class Report {
+
+	private String Date;
+	private String Impressions;
+	private String Clicks;
+	private String Earning;
+	
+	public String getDate() {
+		return Date;
+	}
+	public void setDate(String date) {
+		Date = date;
+	}
+	public String getImpressions() {
+		return Impressions;
+	}
+	public void setImpressions(String impressions) {
+		Impressions = impressions;
+	}
+	public String getClicks() {
+		return Clicks;
+	}
+	public void setClicks(String clicks) {
+		Clicks = clicks;
+	}
+	public String getEarning() {
+		return Earning;
+	}
+	public void setEarning(String earning) {
+		Earning = earning;
+	}
+
+	
+}
