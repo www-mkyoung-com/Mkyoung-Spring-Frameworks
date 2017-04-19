ab175883298680aaef9883a717b448f256370e91
mghasemy <mohammad.ghasemy@gmail.com>
2017-04-19 13:37:34 +0430

    mk5 v7

---
+++ Spring Security Tutorial/FAQs/Spring Security - Customize 403 access denied page/src/main/java/com/mkyong/web/controller/HelloController.java
@@ -0,0 +1,53 @@
+package com.mkyong.web.controller;
+
+import java.security.Principal;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloController {
+
+	@RequestMapping(value = { "/", "/welcome**" }, method = RequestMethod.GET)
+	public ModelAndView welcomePage() {
+
+		ModelAndView model = new ModelAndView();
+		model.addObject("title", "Spring Security Hello World");
+		model.addObject("message", "This is welcome page!");
+		model.setViewName("hello");
+		return model;
+
+	}
+
+	@RequestMapping(value = "/admin**", method = RequestMethod.GET)
+	public ModelAndView adminPage() {
+
+		ModelAndView model = new ModelAndView();
+		model.addObject("title", "Spring Security Hello World");
+		model.addObject("message", "This is protected page!");
+		model.setViewName("admin");
+
+		return model;
+
+	}
+
+	// for 403 access denied page
+	@RequestMapping(value = "/403", method = RequestMethod.GET)
+	public ModelAndView accesssDenied(Principal user) {
+
+		ModelAndView model = new ModelAndView();
+
+		if (user != null) {
+			model.addObject("msg", "Hi " + user.getName() + ", you do not have permission to access this page!");
+		} else {
+			model.addObject("msg", "You do not have permission to access this page!");
+		}
+
+		model.setViewName("403");
+		return model;
+
+	}
+
+}
---
+++ Spring Security Tutorial/FAQs/Spring Security - Customize 403 access denied page/src/main/java/com/mkyong/web/exception/MyAccessDeniedHandler.java
@@ -0,0 +1,40 @@
+package com.mkyong.web.exception;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+public class MyAccessDeniedHandler implements AccessDeniedHandler {
+
+	private String errorPage;
+
+	public MyAccessDeniedHandler() {
+	}
+
+	public MyAccessDeniedHandler(String errorPage) {
+		this.errorPage = errorPage;
+	}
+
+	public String getErrorPage() {
+		return errorPage;
+	}
+
+	public void setErrorPage(String errorPage) {
+		this.errorPage = errorPage;
+	}
+
+	@Override
+	public void handle(HttpServletRequest request, HttpServletResponse response,
+			AccessDeniedException accessDeniedException) throws IOException, ServletException {
+
+		//some business logic
+		response.sendRedirect(errorPage);
+
+	}
+
+}
