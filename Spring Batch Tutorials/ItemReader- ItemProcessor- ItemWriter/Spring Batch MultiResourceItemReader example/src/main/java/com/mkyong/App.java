b79256735226ca0ea21093d42b0409c4b7f0f09d
mghasemy <mohammad.ghasemy@gmail.com>
2017-04-19 16:35:12 +0430

    mk5 v20

---
+++ Spring Batch Tutorials/ItemReader- ItemProcessor- ItemWriter/Spring Batch MultiResourceItemReader example/src/main/java/com/mkyong/App.java
@@ -0,0 +1,43 @@
+package com.mkyong;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		App obj = new App();
+		obj.run();
+
+	}
+
+	private void run() {
+
+		String[] springConfig = { "spring/batch/jobs/job-read-files.xml" };
+
+		ApplicationContext context = new ClassPathXmlApplicationContext(springConfig);
+
+		JobLauncher jobLauncher = (JobLauncher) context.getBean("jobLauncher");
+		Job job = (Job) context.getBean("readMultiFileJob");
+
+		try {
+
+			JobExecution execution = jobLauncher.run(job, new JobParameters());
+			System.out.println("Exit Status : " + execution.getStatus());
+			System.out.println("Exit Status : " + execution.getAllFailureExceptions());
+
+		} catch (Exception e) {
+			e.printStackTrace();
+
+		}
+
+		System.out.println("Done");
+
+	}
+
+}
